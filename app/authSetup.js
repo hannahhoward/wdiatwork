72a2aca07e1a99576c55c71ef5b0af12478bd2ba
Hannah Howard <hannah@hannahhoward.net>
2014-05-06 12:49:03 -0700

    Current state

---
+++ app/authSetup.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(passport, GithubStrategy, BearerStrategy, UserDB) {
+  passport.use('github', new GithubStrategy({
+    clientID: process.env['GITHUB_CLIENT'],
+    clientSecret: process.env['GITHUB_SECRET'],
+    callbackURL: (process.env['SERVER_URL']+'/callback')
+    }, function(accessToken, refreshToken, profile, done) {
+      UserDB.findOrCreate({username: profile.id},
+        {email: profile.email},
+        {accessToken: accessToken}, function(err, user) {
+          done(err, user);
+        });
+    }));
+
+  passport.use('bearer', new BearerStrategy(function(token, done) {
+    UserDB.findOne({accessToken: token}, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      return done(null, user);
+    });
+  }));
+
+}
---
+++ app/models/user.js
@@ -0,0 +1,58 @@
+'use strict';
+
+
+module.exports = function (mongoose, supergoose) {
+
+  var Schema = mongoose.Schema
+
+  /**
+   * A Validation function for local strategy properties
+   */
+
+  var validatePresenceOf = function(property) {
+    return ((this.provider !== 'local' && !this.updated) || property.length);
+  };
+
+  /**
+   * User Schema
+   */
+  var UserSchema = new Schema({
+    firstName: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    accessToken: {
+      type: String
+    },
+    email: {
+      type: String,
+      trim: true,
+      default: '',
+      match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: 'Please fill in a username',
+      trim: true
+    },
+    updated: {
+      type: Date
+    },
+    created: {
+      type: Date,
+      default: Date.now
+    }
+  });
+
+  UserSchema.plugin(supergoose, {});
+
+  return mongoose.model('User', UserSchema);
+
+}
