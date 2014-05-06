fc2fb4fd8b750d377100c8feb0c5177ad1fedfd3
Hannah Howard <hannah@hannahhoward.net>
2014-05-06 12:48:58 -0700

    Current state

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
  // app/routes.js
 
-  module.exports = function(app) {
+  module.exports = function(app, passport) {
 
     // server routes ===========================================================
     // handle things like api calls
@@ -9,10 +9,25 @@
     // route to handle creating (app.post)
     // route to handle delete (app.delete)
 
+
+    app.get('/login',
+      passport.authenticate('github'));
+
+    app.get('/callback',
+      passport.authenticate('github', { failureRedirect: '/login' }),
+        function(req, res) {
+          res.writeHead(302, {
+            'Location': process.env['SERVER_URL']+'/#/index?token=' + req.user.token
+          });
+          res.end();
+        });
+    });
+
     // frontend routes =========================================================
     // route to handle all angular requests
     app.get('*', function(req, res) {
-      res.render('index')
+      res.sendfile('./public/index.html');
     });
+
 
   };
--- server.js
+++ server.js
@@ -3,42 +3,30 @@
   // modules =================================================
   var express = require('express');
   var app     = express();
-  var ejsLocals = require('ejs-locals');
-  var githubOAuth = require('github-oauth')({
-    githubClient: process.env['GITHUB_CLIENT'],
-    githubSecret: process.env['GITHUB_SECRET'],
-    baseURL: process.env['SERVER_URL'],
-    loginURI: '/login',
-    callbackURI: '/callback',
-    scope: 'user, read:org' // optional, default scope is set to user
-  })
-
+  var mongoose= require('mongoose');
+  var passport = require('passport');
+  var supergoose = require('supergoose');
   // configuration ===========================================
 
   // config files
 
+  var db = require('./config/db');
+
   var port = process.env.PORT || 8080; // set our port
-  // mongoose.connect(db.url); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
+
+  mongoose.connect(db.url); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
 
   app.configure(function() {
-    app.engine('ejs', ejsLocals);
-    app.set('views', __dirname + '/app/views');
-    app.set('view engine', 'ejs');
     app.use(express.static(__dirname + '/public'));   // set the static files location /public/img will be /img for users
     app.use(express.logger('dev'));           // log every request to the console
     app.use(express.bodyParser());            // have the ability to pull information from html in POST
     app.use(express.methodOverride());          // have the ability to simulate DELETE and PUT
   });
 
-
-
-  githubOAuth.addRoutes(app, function(err, token, serverResponse, tokenResponse) {
-    if (err == false) {
-      serverResponse.render('token', { token: token });
-    } else {
-      serverResponse.render('index')
-    }
-  })
+  var UserDB = require('./app/models/user')(mongoose, supergoose);
+  var GithubStrategy = require('passport-github').Strategy;
+  var BearerStrategy = require('passport-http-bearer').Stategy;
+  require('./app/authSetup')(passport, GithubStrategy, BearerStrategy, UserDB);
 
   // routes ==================================================
   require('./app/routes')(app); // configure our routes
